50f5804df5347f2c503de902e689431d483f8781
Justin Law <justinlaw801@gmail.com>
2021-07-23 19:26:45 -0400

    added content to about and content pages

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,6 +6,12 @@ function About() {
         <div className="create-content">
             <div className='create-card'>
                 <h3>About The App</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet ornare elit, nec pharetra quam malesuada quis. Vivamus vulputate felis ac quam facilisis molestie. Mauris tincidunt, neque vel rhoncus dapibus, libero augue ultrices lorem, eu tincidunt dolor metus ut est. Quisque at vulputate sem, ut eleifend nunc. Nam ullamcorper risus sit amet blandit varius. Mauris in nisl vel libero porttitor dictum eget id urna. Sed purus lacus, scelerisque aliquam tellus non, bibendum imperdiet sem.</p>
+                    
+                    <p>Aenean non quam augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer porta sit amet justo quis cursus. Donec euismod erat vel ullamcorper bibendum. Vivamus magna elit, consectetur et est at, ultrices finibus diam. Pellentesque orci ligula, interdum sed sapien at, elementum cursus augue. Donec et lacus id neque iaculis gravida eget ac tellus. Vivamus mauris dolor, mattis non massa ultrices, accumsan sagittis tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    
+                    <p>Quisque eu posuere nunc, a ullamcorper purus. Vestibulum vitae congue tellus. Aliquam scelerisque, nisl id egestas faucibus, justo metus auctor dolor, vitae imperdiet turpis felis ac nisl. Nunc lacus magna, fermentum nec iaculis a, pellentesque vitae nisl. Maecenas gravida mauris ut ullamcorper viverra. Ut eu enim sodales, molestie quam vel, dictum arcu. In id enim efficitur, facilisis massa laoreet, aliquam ligula. Mauris vulputate fermentum dui eu semper. Nunc gravida pharetra lectus, nec tincidunt nibh maximus eget.</p>
+                    
             </div>
         </div>
     )
--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -6,6 +6,19 @@ function Contact() {
         <div className="create-content">
             <div className='create-card'>
                 <h3>Contact Me</h3>
+                <br></br>
+                <ul style={{listStyleType: 'none'}}>
+                    <li>
+                        <div>Email: justinlaw801@gmail.com</div>
+                    </li>
+                    <li>
+                        <a href="https://github.com/justin-law" target="_blank">Github</a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/justinlaw801/" target="_blank">LinkedIn</a>
+                    </li>
+                </ul>
+                
             </div>
         </div>
     )
